f0c879a9f80787beb532959de868228fb6edfc24
ak9250 <ahsen.khaliq@gmail.com>
2020-12-31 12:47:17 -0500

    changes

--- runway_model.py
+++ runway_model.py
@@ -3,12 +3,13 @@ import numpy as np
 import argparse
 import torch
 from run import restore
+from PIL import Image
 
 @runway.command('translate', inputs={'source_imgs': runway.image(description='input image to be translated'),}, outputs={'image': runway.image(description='input image to be translated')})
 def translate(model, inputs):
     image = restore(inputs['source_imgs'], True)
-    return image
-
+    im = Image.open(open(image, 'rb'))
+    return im
 
 if __name__ == '__main__':
     runway.run(port=8889)
