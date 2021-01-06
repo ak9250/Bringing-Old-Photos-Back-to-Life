dd606d4042d58c010ebb17c96744fee579980d23
ak9250 <ahsen.khaliq@gmail.com>
2021-01-06 12:06:09 -0500

    no scratch remove

--- runway_model.py
+++ runway_model.py
@@ -5,7 +5,7 @@ import torch
 from run import restore
 from PIL import Image
 
-@runway.command('translate', inputs={'source_imgs': runway.image(description='input image to be translated'),'scratch_remove': runway.boolean(default=True,description='remove scratches'),}, outputs={'image': runway.image(description='out image after restoration')})
+@runway.command('translate', inputs={'source_imgs': runway.image(description='input image to be translated'),'scratch_remove': runway.boolean(default=False,description='remove scratches'),}, outputs={'image': runway.image(description='out image after restoration')})
 def translate(model, inputs):
     image = restore(inputs['source_imgs'], inputs['scratch_remove'])
     im = Image.open(open(image, 'rb'))
