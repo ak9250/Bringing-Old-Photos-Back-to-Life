885df812c2c35873f01b3f46fccbe5c1701639f3
ak9250 <ahsen.khaliq@gmail.com>
2021-01-13 10:43:39 -0500

    desc

--- runway_model.py
+++ runway_model.py
@@ -2,7 +2,7 @@ import runway
 from run import restore
 from PIL import Image
 
-@runway.command('translate', inputs={'source_imgs': runway.image(description='input image to be translated'),'scratch_remove': runway.boolean(default=False,description='remove scratches'),}, outputs={'image': runway.image(description='out image after restoration')})
+@runway.command('translate', inputs={'source_imgs': runway.image(description='input image to be translated'),'scratch_remove': runway.boolean(default=False,description='remove scratches'),}, outputs={'image': runway.image(description='output image after restoration')})
 def translate(model, inputs):
     image = restore(inputs['source_imgs'], inputs['scratch_remove'])
     im = Image.open(open(image, 'rb'))
